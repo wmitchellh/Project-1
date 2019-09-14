75ecb039934c70bdbea7095735dffc0186d25fe6
wmitchellh <54546794+wmitchellh@users.noreply.github.com>
2019-09-14 19:33:32 -0400

    09/14 - 7:33pm

---
+++ Project1/js/project1.js
@@ -0,0 +1,7 @@
+function newPicture() {
+  document.getElementById("dash1").src="assets/icon-dashboard-a.png";
+}
+
+function oldPicture() {
+  document.getElementById("dash1").src="assets/icon-dashboard.png";
+}
